7fc90b00095acdd225573391889d54cd01802be4
zoefisk <94054962+zoefisk@users.noreply.github.com>
2026-03-22 13:24:31 -0400

    feat: add a page for debug testing
    currently, it is testing the StudyQuestionsSet component

---
+++ src/app/testing/page.tsx
@@ -0,0 +1,13 @@
+import StudyQuestionsSet from "@/components/ui/StudyQuestionsExample";
+import PageShell from "@/components/layout/PageShell";
+import {SET_ONE, VIZ_QUESTIONS} from "@/data/caseStudyQuestions";
+import {Divider} from "@mui/material";
+
+export default function TestingPage() {
+
+    return (
+        <PageShell>
+            <StudyQuestionsSet questions={VIZ_QUESTIONS}/>
+        </PageShell>
+    );
+}
