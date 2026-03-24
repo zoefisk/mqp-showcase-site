d07b61c3315b21dc8e482f95610c79d8b3b44ab5
zoefisk <zoejewelfisk@gmail.com>
2026-03-24 17:01:13 -0400

    feat: add MDX support to Next.js app (install @next/mdx and configure next.config)

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+import createMDX from '@next/mdx'
+
+const withMDX = createMDX({
+    extension: /\.mdx?$/,
+})
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
+}
+
+export default withMDX(nextConfig)
