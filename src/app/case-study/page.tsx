5b7284bf67816615e9335a212bac0fc7f76fbcb0
zoefisk <zoejewelfisk@gmail.com>
2026-04-14 21:57:45 -0400

    feat: create python viewer for case study

--- src/app/case-study/page.tsx
+++ src/app/case-study/page.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
-import { Box, Container } from "@mui/material";
+import {Box, Container, Typography} from "@mui/material";
 
 import PageHeaderContent from "@/content/case-study/PageHeader.mdx";
 import StudySnapshotContent from "@/content/case-study/StudySnapshot.mdx";
 import ProjectConnectionContent from "@/content/case-study/ProjectConnection.mdx";
 import StudyQuestionsContent from "@/content/case-study/StudyQuestions.mdx";
 import SpecificationPreviewContent from "@/content/case-study/SpecificationPreview.mdx";
+import PythonCodeViewer from "@/components/ui/PythonCodeViewer";
+import InlineCode from "@/components/ui/InlineCode";
 
 export default function CaseStudyPage() {
     return (
@@ -22,6 +24,19 @@ export default function CaseStudyPage() {
                 <ProjectConnectionContent />
                 <StudyQuestionsContent />
                 <SpecificationPreviewContent />
+
+                <PythonCodeViewer
+                    src="/python/case-study/featureEngineering.py"
+                    label="Feature Engineering"
+                    filename="featureEngineering.py"
+                    defaultSoftWrap={true}
+                />
+
+                {/*<Typography>*/}
+                {/*    Use <InlineCode>process_data(df, threshold=0.5)</InlineCode> to begin.*/}
+                {/*</Typography>*/}
+
+
             </Container>
         </Box>
     );
